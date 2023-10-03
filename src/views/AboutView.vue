53760bf82ccb6105ea45b287b62da231edbada0a
Auriel James <aurieljames11@gmail.com>
2023-10-03 23:40:27 +0800

    issue/#7 Added only component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import SkillsView from '@/components/SkillsView.vue'
 import { useRouter } from 'vue-router'
 const router = useRouter();
 
 </script>
-
 <template>
+  
   <main>
     <div class="about w-4/6 ml-auto mr-auto mt-10 bg-neutral-800 pl-5 pr-4 pt-5 pb-5 rounded-xl">
-      <p class="text-white font-bold text-lg pb-4 uppercase pl-4">About me</p><hr class="border-1 border-red-600 border">
+      <p class="text-white font-bold text-lg pb-4 uppercase pl-4">About me</p><hr class="border-1 border-yellow-300 border">
       <div class="flex text-white items-center justify-around pt-4">
         <img src="@/assets/25.jpg" class="h-[250px] w-[250px] rounded-full">
         <div class="max-w-50 w-1/2 break-words space-y-6 text-sm">
@@ -27,13 +28,14 @@ const router = useRouter();
     </div>
     <div class="w-4/6 ml-auto mr-auto">
       <div class="flex pt-10 font-semibold items-center">
-        <hr class="border border-1 border-red-600 w-20">
+        <hr class="border border-1 border-yellow-300 w-20">
         <p class="text-white pl-3 text-xl">Skills</p>
       </div>
-      <div class="space-x-2 pt-2">
-        <RouterLink to="/about/techstack" class="text-white text-semibold">Tech Stack</RouterLink>
-        <RouterLink to="/about/tools" class="text-white text-semibold">Tools</RouterLink>
+      <div class="space-x-2 pt-3">
+        <button class="text-white font-semibold hover:bg-neutral-800 focus:bg-neutral-800 pt-2 hover:rounded-sm focus:rounded-sm pb-2 pr-3 pl-3 focus:text-yellow-600 hover:text-yellow-600 animation duration-300">Tech Stack</button>
+        <button class="text-white font-semibold hover:bg-neutral-800 focus:bg-neutral-800 pt-2 hover:rounded-sm focus:rounded-sm pb-2 pr-3 pl-3 focus:text-yellow-600 hover:text-yellow-600 animation duration-300">Tools</button>
       </div>
+      <SkillsView />
     </div>
   </main>
 </template>
